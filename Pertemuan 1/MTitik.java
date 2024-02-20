c6ac53ef1a8172f27580a16cf79988f41421915c
rizaladiyanto <118915120+rizaladiyanto@users.noreply.github.com>
2024-02-20 11:23:08 +0700

    Add files via upload

---
+++ Pertemuan 1/MTitik.java
@@ -0,0 +1,21 @@
+public class MTitik {
+    public static void main(String[] args) {
+       //deklarasi objek t1 dan t2 dari kelas Titik
+       Titik t1 = new Titik();
+       t1.setAbsis(1.0);
+       t1.setOrdinat(2.0);
+       
+       Titik t2 =  new Titik();
+       t2.setAbsis(3.0);
+       t2.setOrdinat(4.0);
+
+       Titik t3 = new Titik();
+       t3.setAbsis(5.0);
+       t3.setOrdinat(6.0);
+       
+       System.out.println("Jumlah objek titik : " + t1.getCounter());
+       System.out.println("t1 (" + t1.getAbsis() + ", " + t1.getOrdinat() + ") ");
+       System.out.println("t2 (" + t2.getAbsis() + ", " + t2.getOrdinat() + ") ");
+       System.out.println("t3 (" + t3.getAbsis() + ", " + t3.getOrdinat() + ") ");
+    }
+}
---
+++ Pertemuan 1/Titik.java
@@ -0,0 +1,34 @@
+class Titik{
+    // deklarasi atribut
+    double absis;
+    double ordinat;
+    static int counterTitik;
+
+    // deklarasi konstruktor
+    Titik(){
+         absis = 0;
+         ordinat = 0;
+         counterTitik++;
+    }
+
+    // deklarasi metode 
+    void setAbsis(double a){
+        absis = a;
+    }
+
+    void setOrdinat(double o){
+        ordinat = o;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    int getCounter(){
+        return counterTitik;
+    }
+}
